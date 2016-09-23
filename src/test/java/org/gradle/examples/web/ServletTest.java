3de75a63d9ad1fe3e5f97b9a114a084dddaa7a70
Napo2k <Napo2k@users.noreply.github.com>
2016-09-23 12:43:43 +0200

    Changed to use Mockit oinstead

--- src/test/java/org/gradle/examples/web/ServletTest.java
+++ src/test/java/org/gradle/examples/web/ServletTest.java
@@ -1,37 +1,27 @@
 package org.gradle.examples.web;
 
 import static org.junit.Assert.*;
-
+import static org.mockito.Mockito.*;
+import java.io.*;
+import javax.servlet.http.*;
 import org.junit.*;
-import org.mortbay.jetty.testing.HttpTester;
-import org.mortbay.jetty.testing.ServletTester;
+import org.mockito.Mockito;
 
-import org.gradle.examples.web.ServletTesterUtils;
-
-public class ServletTest
-{
-	@Test
+public class ServletTest extends Mockito {
+	
+    @Test
 	public void testGetRequest() throws Exception {
-		System.out.println("Starting");
-		ServletTester servletTester = new ServletTester();
-		servletTester.addServlet(org.gradle.examples.web.Servlet.class, "/test");
-		servletTester.start();
-		
-		System.out.println("Sending request");
-		
-		HttpTester request = new HttpTester(); 
-    	request.setMethod("GET");
-    	request.setURI("/test");
-    	request.setVersion("HTTP/1.0");
-		
-		HttpTester response = new HttpTester();
-		response.parse(servletTester.getResponses(request.generate()));
-		
-		System.out.println("Handling response");
-		assertEquals("hello, world",response.getContent());
-	}
-	/*
-	@Test public void testNull() throws Exception {
-		assertEquals("OK","OK");
-	}*/
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        
+        StringWriter sw = new StringWriter();
+        PrintWriter writer = new PrintWriter(sw);
+        
+        when(response.getWriter()).thenReturn(writer);
+        
+        new Servlet().doGet(request, response);
+        
+        writer.flush();
+        assertEquals("hello, world\r\n", sw.toString());
+    }
 }
